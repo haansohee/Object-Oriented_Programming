367fcaff9b1382ed28577ece434f135752105a2e
한소희 <hansohee@hansohuiui-MacBookPro.local>
2022-07-13 19:09:04 +0900

    java_week1

---
+++ week1/src/week1/Hello.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("학과 : 컴퓨터정보과");
+		System.out.println("학번 : 202144020");
+		System.out.println("이름 : 한소희");
+
+	}
+
+}
