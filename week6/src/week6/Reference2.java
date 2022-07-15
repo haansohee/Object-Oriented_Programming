011f5b5d3bf629cf344b2c0754eb63dd5b7af9aa
haansohee <bearforlove@naver.com>
2022-07-15 20:19:27 +0900

    java_week6

---
+++ week6/src/week6/Reference2.java
@@ -0,0 +1,27 @@
+package week6;
+
+public class Reference2 {
+
+	public static void main(String[] args) {
+		// 자바는 문자열 리터럴이 동일하다면 String 객체를 공유함
+		String sVal1 = "Java";
+		String sVal2 = "Java";
+		System.out.println("sVal1 == sVal2 : " + (sVal1 == sVal2 ? "참조가 같음" : "참조가 다름"));
+		
+		// new 연산자를 이용하는 경우에는 힙 영역에 새로운 String 객체가 생성됨
+		String sVal3 = new String("Java");
+		String sVal4 = new String("Java");
+		System.out.println("sVal3 == sVal4 : " + (sVal3 == sVal4 ? "참조가 같음" : "참조가 다름"));
+		
+		// String 객체가 동일한 문자열을 가지고 있는지 비교할 경우에는 
+		// 반드시 equals() 메소드 이용
+		if (sVal3.equals(sVal4)) {
+			System.out.println("sVal3와 sVal4의 문자열은 동일");
+		}
+		else {
+			System.out.println("sVal3와 sVal4의 문자열은 다름");	
+		}
+		
+	}
+
+}
