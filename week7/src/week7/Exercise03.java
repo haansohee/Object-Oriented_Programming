d5759062112ebc0d2a693d4724fe09b8d998e4ff
haansohee <bearforlove@naver.com>
2022-07-16 23:33:24 +0900

    java_week7

---
+++ week7/src/week7/Exercise03.java
@@ -0,0 +1,15 @@
+package week7;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+
+}
